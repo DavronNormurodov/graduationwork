c44167f5586f5e8e80ae6f412d878d8142aafc73
davron <mrz.davron@gmail.com>
2022-06-18 19:23:24 +0500

    set default image size

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,10 +1,12 @@
+from PIL import Image
 from django.db.models import Model,\
                             JSONField,\
                             FloatField,\
                             ImageField,\
                             DateTimeField,\
                             ForeignKey,\
-                            SET_NULL
+                            SET_NULL,\
+                            CharField
 
 
 def upload_to(instance, filename):
@@ -20,6 +22,9 @@ class Category(Model):
 
 
 class Product(Model):
+    # image_height = CharField(max_length=10, null=True, blank=True, editable=False, default="300")
+    # image_width = CharField(max_length=10, null=True, blank=True, editable=False, default="300")
+
     price = FloatField(default=0)
     title = JSONField()
     image = ImageField(upload_to=upload_to, null=True)
@@ -27,6 +32,17 @@ class Product(Model):
 
     category = ForeignKey(Category, SET_NULL, 'products', null=True)
     created_at = DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.image:
+            return
+
+        super().save()
+        image = Image.open(self.image)
+        (width, height) = image.size
+        size = (300, 300)
+        image = image.resize(size, Image.ANTIALIAS)
+        image.save(self.image.path)
 
     def __str__(self):
         return self.title['uz']
